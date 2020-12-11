0fe1ae4c4c6812938d0daffd53461dd6a76a1454
Furkan <furkan_bfk@hotmail.com>
2020-12-11 18:08:31 +0300

    Initial Commit

---
+++ webviewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webviewer
+//
+//  Created by Furkan KESGIN on 12/11/20.
+//  Copyright © 2020 Furkan KESGIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
