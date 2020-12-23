22b5a80cff52101ee15e048986f7ea926831c3c9
Furkan <furkan_bfk@hotmail.com>
2020-12-23 19:37:51 +0300

    android webviewer added

---
+++ android/webview/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = (WebView)findViewById(R.id.webview);
+        view.setWebViewClient(new WebViewClient());
+        view.loadUrl("https://www.google.com");
+    }
+}
